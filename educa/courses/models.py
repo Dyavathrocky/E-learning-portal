3fc4e0665bb2062b0f240ae37787d20056a76841
rakesh <davathrak@gmail.com>
2020-12-10 10:31:51 +0530

    Changes

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import  ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 # Create your models here.
 
@@ -35,9 +36,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
@@ -49,9 +54,13 @@ class Content(models.Model):
                                      'video',
                                      'image',
                                      'file')})
-                                     
+
     object_type = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_type')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
@@ -60,7 +69,7 @@ class ItemBase(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    class Meta():
+    class Meta:
         abstract = True
 
     def __str__(self):
